f510049c2f76a963ef7ec87958673e77c23a6f85
Sergio <sergiocmps7@gmail.com>
2021-09-12 21:45:54 -0600

    deploy

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,5 @@ const apiRoutes = require("./api/books");
 router.use("/api", apiRoutes);
 
 // If no API routes are hit, send the React app
-router.use(function(req, res) {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
 
 module.exports = router;
